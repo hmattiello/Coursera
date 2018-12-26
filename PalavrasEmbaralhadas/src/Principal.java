8a22506dae8f3ff5a40b85613a8fc3684c4c114a
hmattiello <hmattiello@HM-Linux-Casa>
2018-12-26 17:22:41 -0200

    Palavras embaralhadas

---
+++ PalavrasEmbaralhadas/src/Principal.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Digite o seu nome!");
+		String nome = in.nextLine();
+		System.out.println("O seu nome é: "+nome);
+	}
+
+}
